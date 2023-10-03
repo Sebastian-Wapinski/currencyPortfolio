6ff4902b89513431e695daeea2c5bda88579e071
Sebastian <sebastian.pawel.wapinski@gmail.com>
2023-10-03 18:13:38 +0200

    config

---
+++ src/components/style/theme.js
@@ -0,0 +1,11 @@
+export const theme = {
+  primaryBackground: '#FAF8F4',
+  secondaryBackground: '#DCFAEB',
+  thirdBackground: '#F0DCFA',
+  forthBackground: '#A89CAD',
+  primaryTextColor: '#ADA699',
+  secondaryTextColor: '#7A756C',
+  thirdTextColor: '#474540'
+}
+
+export default theme
